5766ba3e565d86d4fbc729427092f03b2ec04d7b
Igor <78171614+montekrist0@users.noreply.github.com>
2021-11-20 17:51:01 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Contacts from "/src/contacts.js";
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.name}</h2>
+        <img className="circle-img" src={props.imgURL} alt="avatar_img" />
+      </div>
+      <div className="bottom">
+        <p className="info">{props.phone}</p>
+        <p className="info">{props.email}</p>
+      </div>
+    </div>
+  );
+}
+
+function CardList(contacts) {
+  const cards = contacts.cards;
+  return (
+    <div>
+      {cards.map((card) => (
+        <Card
+          key={card.phone}
+          name={card.name}
+          imgURL={card.imgURL}
+          phone={card.phone}
+          email={card.email}
+        />
+      ))}
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <CardList cards={Contacts} />
+    </div>
+  );
+}
+
+export default App;
