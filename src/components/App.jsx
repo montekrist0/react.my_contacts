121ff2d1b3baf10f50da0e189028ca7c68195c30
Igor <78171614+montekrist0@users.noreply.github.com>
2021-11-20 20:55:48 +0300

    Add new definition of map for cards

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,21 +3,29 @@ import Contacts from "/src/contacts.js";
 import Avatar from "./Avatar";
 import Card from "./Card";
 
-function CardList(contacts) {
-  const cards = contacts.cards;
+function createCard(contact) {
   return (
-    <div>
-      {cards.map((card) => (
-        <Card
-          key={card.phone}
-          name={card.name}
-          imgURL={card.imgURL}
-          phone={card.phone}
-          email={card.email}
-        />
-      ))}
-    </div>
+    <Card
+      key={contact.phone}
+      name={contact.name}
+      imgURL={contact.imgURL}
+      phone={contact.phone}
+      email={contact.email}
+    />
   );
+}
+
+function CardList(contacts) {
+  // const cards = contacts.cards;
+  return contacts.cards.map((card) => (
+    <Card
+      key={card.phone}
+      name={card.name}
+      imgURL={card.imgURL}
+      phone={card.phone}
+      email={card.email}
+    />
+  ));
 }
 
 function App() {
@@ -26,6 +34,7 @@ function App() {
       <h1 className="heading">My Contacts</h1>
       <Avatar url="https://sun9-86.userapi.com/impg/bwRLt9PSuLalQ3QMmTU7qUdwziP48kBnL9Jomw/Qo6LdBJxM2I.jpg?size=1620x2160&quality=96&sign=14e17e53d2f56bf91b0b44242e60fe3b&type=album" />
       <CardList cards={Contacts} />
+      {Contacts.map(createCard)}
     </div>
   );
 }
